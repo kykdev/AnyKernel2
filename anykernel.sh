f3159500ef0aa6f470e290def40d997e7fc833e3
maplezeon <kykint@naver.com>
2019-07-18 19:24:26 +0900

    cpufreq-limiter

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=DirtyV by bsmitty83 @ xda-developers
-do.devicecheck=1
+kernel.string=cpufreq limiter for hero by kykint
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/155a0000.ufs/by-name/BOOT;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -38,22 +38,13 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+insert_file init.rc "Limit cpufreq by kykint" after "chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/io_is_busy" cpufreq_limit
+replace_string fstab.samsungexynos8890 encryptable "ro,errors=panic,noload        " "ro,noatime,errors=panic,noload"
+replace_string fstab.samsungexynos8890.fwup encryptable "ro,errors=panic,noload        " "ro,noatime,errors=panic,noload"
+replace_string fstab.samsungexynos8890 encryptable "wait,verify" wait
+replace_string fstab.samsungexynos8890.fwup encryptable "wait,verify" wait
+replace_string fstab.samsungexynos8890 encryptable forceencrypt encryptable
+replace_string fstab.samsungexynos8890.fwup encryptable forceencrypt encryptable
 
 # end ramdisk changes
 
